4ecee57cd80fcf211c07619c2df628f75ceef0e3
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-10-25 20:35:51 +0200

    remove comments

--- script.js
+++ script.js
@@ -57,7 +57,6 @@ function scoreChecker() {
         resetbtn.addEventListener('click', reset);
         results.appendChild(resetbtn);
 
-        // Maybe add some game reset logic here
     }
 }
 
@@ -70,21 +69,10 @@ function reset() {
     results.removeChild(resetbtn);
 }
 
-/* function game() {
-    const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase()
-    const computerSelection = getComputerChoice()
-    return playRound(playerSelection, computerSelection)
-    return playRound(playerSelection, computerSelection)
-} */
-
-/* console.log(game()); */
-
 let btns = document.querySelector('#buttons');
 
 btns.addEventListener('click', (event) => {
     let target = event.target;
-
-    // console.log(target);
 
     switch(target.id) {
         case 'rock':
@@ -94,12 +82,3 @@ btns.addEventListener('click', (event) => {
             break;
         }
     });
-
-
-
-/* Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). */
-
-
-
-/* Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step! */
